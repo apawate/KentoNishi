faa12fb6942884d670a62d55ae57c63d74778ae8
Agastya Pawate <73004127+apawate@users.noreply.github.com>
2021-05-27 10:24:04 -0700

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,19 +1,24 @@
 import os
 import time
 os.system("clear")
-for x in range(5):
-  print("BIOS Revision KENT0")
-  for y in range(x):
-    print("-")
-  os.system("clear")
-  time.sleep(0.5)
+print("BIOS Revision KENT0")
+time.sleep(0.5)
+os.system("clear")
+print("loading")
+time.sleep(0.5)
 os.system("clear")
 print("GNU GRUB version k3n+0: ")
 print("*KentOS")
 print("   command prompt on dev/die-hard-keyboard-users")
 choice = input("Enter first letter of your choice: ")
 time.sleep(0.5)
-if choice = "c":
+os.system("clear")
+print("Entering command line mode")
+os.system("clear")
+if choice == "c":
   while 1:
     cmd = input("")
     print(os.system(cmd))
+elif choice == "k" or choice == "K":
+    print("Welcome to KentOS!")
+    
