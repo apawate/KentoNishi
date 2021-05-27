a4d51be3fe517ef3f1f9b66d0f0587287019a3c6
Agastya Pawate <73004127+apawate@users.noreply.github.com>
2021-05-27 10:15:39 -0700

    Create boot.py

---
+++ boot.py
@@ -0,0 +1,19 @@
+import os
+import time
+os.system("clear")
+for x in range(5):
+  print("BIOS Revision KENT0")
+  for y in range(x):
+    print("-")
+  os.system("clear")
+  time.sleep(0.5)
+os.system("clear")
+print("GNU GRUB version k3n+0: ")
+print("*KentOS")
+print("   command prompt on dev/die-hard-keyboard-users")
+choice = input("Enter first letter of your choice: ")
+time.sleep(0.5)
+if choice = "c":
+  while 1:
+    cmd = input("")
+    print(os.system(cmd))
